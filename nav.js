666e2b3e8b65b07ac7f4c45e35c84d56e5cadb2c
lcondon14 <122494018+lcondon14@users.noreply.github.com>
2023-07-23 15:36:20 -0500

    update

--- nav.js
+++ nav.js
@@ -13,16 +13,17 @@ class Nav extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <div class="nav-header">
-            <img src="">
+            <img src="./imgs/portfolio-header.png">
             </div>
         <nav id="navBar">
-            <ul>
-                <li><a href="index.html">home</a></li>
-                <li><a href="about.html">about</a></li>
+            <ul class="navList>
+                <li><a href="bucket.html">about</a></li>
+                <li><a href="about.html">home</a><li>
                 <li><a href="projects.html">projects</a></li>
                 <li><a href="api-testing.html">api testing</a></li>
                 <li><a href="data-diagrams.html">database diagrams</a></li>
                 <li><a href="resume.html">resume</a></li>
+        
             </ul>
             </nav>
       ` ;
