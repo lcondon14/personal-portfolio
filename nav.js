09ef3aba400bb766e8e87acbfed887444ebddf30
lcondon14 <122494018+lcondon14@users.noreply.github.com>
2023-11-18 16:07:31 -0600

    update for week 5

--- nav.js
+++ nav.js
@@ -17,8 +17,9 @@ class Nav extends HTMLElement {
             </div>
         <nav id="navBar">
             <ul class="navList>
-                <li><a href="bucket.html">about</a></li>
-                <li><a href="about.html">home</a><li>
+                <li><a href="index.html"></a><li>
+                <li><a href="index.html">home</a><li>
+                <li><a href="about.html">about</a></li>
                 <li><a href="projects.html">projects</a></li>
                 <li><a href="api-testing.html">api testing</a></li>
                 <li><a href="data-diagrams.html">database diagrams</a></li>
