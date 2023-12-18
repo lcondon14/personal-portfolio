583c875ead17f1fc3b7bfe1a9a2a7636af69ba75
lcondon14 <122494018+lcondon14@users.noreply.github.com>
2023-12-17 23:36:20 -0600

    add week 9 presentation + minor code changes

--- web-430/nav-prest.js
+++ web-430/nav-prest.js
@@ -17,13 +17,14 @@ class Nav extends HTMLElement {
             </div>
         <nav id="navBar">
             <ul class="navList>
-                <li><a href="../bucket.html">about</a></li>
-                <li><a href="../about.html">home</a><li>
-                <li><a href="../projects.html">projects</a></li>
-                <li><a href="../api-testing.html">api testing</a></li>
-                <li><a href="../data-diagrams.html">database diagrams</a></li>
-                <li><a href="../resume.html">resume</a></li>
-        
+            <li><a href="../index.html"></a><li>
+            <li><a href="../index.html">home</a><li>
+            <li><a href="../about.html">about</a></li>
+            <li><a href="../projects.html">projects</a></li>
+            <li><a href="../api-testing.html">api testing</a></li>
+            <li><a href="../data-diagrams.html">database diagrams</a></li>
+            <li><a href="../resume.html">resume</a></li>
+    
             </ul>
             </nav>
       ` ;
